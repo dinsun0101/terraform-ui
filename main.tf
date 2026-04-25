a317dd549ef8b2defe9415252510e66637555df0
dinsun0101 <din.sun@live.com>
2026-04-25 21:09:46 +0530

    auto-sync: update main.tf

--- main.tf
+++ main.tf
@@ -96,7 +96,7 @@ module "s3" {
   source = "./modules/s3"
 
   environment = var.environment
-  bucket_name = "my-ui-aro-fixed01"
+  bucket_name = "hello-ui-bkt01"
 }
 
 # ── IAM ────────────────────────────────────────────────────────────────────
