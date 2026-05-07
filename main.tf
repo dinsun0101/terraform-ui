ecee5c95318b25c7305a110e884339be4916bb57
dinsun0101 <din.sun@live.com>
2026-05-07 20:20:08 +0530

    auto-sync: update main.tf

--- main.tf
+++ main.tf
@@ -96,7 +96,7 @@ module "s3" {
   source = "./modules/s3"
 
   environment = var.environment
-  bucket_name = "hello-dinesh-run01"
+  bucket_name = "hello-dinesh-7-may"
 }
 
 # ── IAM ────────────────────────────────────────────────────────────────────
